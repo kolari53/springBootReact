adcb899c310624b703323e0a0f77660a0c18b09d
Deniz Berk Bozkurt <74145056+kolari53@users.noreply.github.com>
2022-03-16 14:26:21 +0300

    eeeeeeeee

---
+++ src/main/java/com/reactSpring/springProjectBackend/SpringProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.reactSpring.springProjectBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reactSpring/springProjectBackend/SpringProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reactSpring.springProjectBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProjectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
